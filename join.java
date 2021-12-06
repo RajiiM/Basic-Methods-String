637ddcbec546753f1f7a6b6a2f5a5a1263d74d1f
RajiiM <mrajalakshmiofficial@gmail.com>
2021-12-06 14:59:53 +0530

    FirstCommit

---
+++ join.java
@@ -0,0 +1,14 @@
+public class Practice {
+
+public static void main(String[] args) {
+System.out.println(String.join("/","18","08","2008"));
+System.out.println(String.join("-","18","08","2008"));
+System.out.println(String.join(":","14", "00"));
+}
+}
+
+
+//OUTPUT
+18/08/2008
+18-08-2008
+14:00
